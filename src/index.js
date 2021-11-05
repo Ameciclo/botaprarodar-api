262abb981962b3c17d215414c3a966c6d687b5c0
analuizasb2 <analuizasb2@gmail.com>
2021-11-05 17:37:12 -0300

    reverting error at build

--- src/index.js
+++ src/index.js
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
 
 const APP_PORT = process.env.PORT || 3000;
 app.listen(APP_PORT, () => {
-  consolcde.log(`Servidor iniciado na porta ${APP_PORT}`);
+  console.log(`Servidor iniciado na porta ${APP_PORT}`);
 });
