64b9646819c872825248aec472a2dc9ad65e2055
analuizasb2 <analuizasb2@gmail.com>
2021-11-05 17:32:38 -0300

    configuring heroku port

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   res.send('O servidor ta on!');
 });
 
-const APP_PORT = 3000;
+const APP_PORT = process.env.PORT || 3000;
 app.listen(APP_PORT, () => {
   console.log(`Servidor iniciado na porta ${APP_PORT}`);
 });
