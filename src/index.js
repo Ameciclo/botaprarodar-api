3f274e97a35d32023c78ec456d471e43d9d4ac5a
analuizasb2 <analuizasb2@gmail.com>
2021-11-08 11:33:16 -0300

    Testing with emptty test file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('O servidor ta on com teste falhando!');
+  res.send('O servidor ta on!');
 });
 
 const APP_PORT = process.env.PORT || 3000;
--- src/index.test.js
+++
@@ -1,7 +0,0 @@
-it('Testing jest', () => {
-  expect(42).toBe(42);
-});
-
-it('Failing test', () => {
-  expect(42).toBe(43);
-});
