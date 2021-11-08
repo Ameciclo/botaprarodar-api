ec755afeae6f66f1fd292118c82c5cdfbaad3e82
analuizasb2 <analuizasb2@gmail.com>
2021-11-08 11:48:17 -0300

    Testing deploy on production

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('O servidor ta on!');
+  res.send('O servidor de producao ta on!');
 });
 
 const APP_PORT = process.env.PORT || 3000;
