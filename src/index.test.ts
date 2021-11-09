16d81de066e35a7915933ead0e45b2eabc9c2f6d
analuizasb2 <analuizasb2@gmail.com>
2021-11-09 10:52:17 -0300

    Excluded test files from build folder, allowed javascript files and added coverage to jest script

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,11 @@
-it('Testing jest', () => {
-  expect(42).toBe(42);
+it('Should pass', () => {
+  expect(1).toBe(1);
 });
+
+// it('Should return message that server is on', async (done) => {
+//   const response = await request.get('/');
+
+//   expect(response.status).toBe(200);
+//   expect(response.body.message).toBe('O servidor ta on!');
+//   done();
+// });
