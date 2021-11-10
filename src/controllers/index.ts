d3bbd0e870b7d7179469418a69d7a24aa5fce806
analuizasb2 <analuizasb2@gmail.com>
2021-11-10 11:36:11 -0300

    Removed reference to deleted file

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import apiStatusController from './ApiStatusController';
-import systemUserController from './SystemUserController';
 
 const routes = Router();
 
 routes.use('/', apiStatusController);
-routes.use('/systemUser', systemUserController);
 
 export default routes;
