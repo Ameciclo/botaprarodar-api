cb983db2bde03458a4d928593cbc15ac4e75e515
analuizasb2 <analuizasb2@gmail.com>
2021-11-10 12:00:02 -0300

    added first test

--- src/controllers/ApiStatusController.test.ts
+++ src/controllers/ApiStatusController.test.ts
@@ -1,3 +1,9 @@
-it('Should pass', () => {
-  expect(1).toBe(1);
+import app from '../index';
+import supertest from 'supertest';
+const request = supertest(app);
+
+it('Should return message that server is on', async (done) => {
+  const response = await request.get('/');
+  expect(response.status).toBe(200);
+  expect(response.body).toBe('O servidor esta rodando...');
 });
--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,5 @@ const APP_PORT = process.env.PORT || 3000;
 app.listen(APP_PORT, () => {
   console.log(`Servidor iniciado na porta ${APP_PORT}`);
 });
+
+export default app;
